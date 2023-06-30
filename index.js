b82694a52fcbcd8d1fc2ae910e281784cf87709b
varunp789 <varun.creole@gmail.com>
2023-06-30 15:59:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import { Configuration, OpenAIApi } from "openai";
+import readline from "readline";
+import express from "express";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const configuration = new Configuration({
+  organization: process.env.OPENAI_API_ORGANIZATION,
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const userInterface = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+userInterface.prompt();
+
+userInterface.on("line", async (input) => {
+  await openai
+    .createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: input }],
+    })
+    .then((res) => {
+      console.log(res.data.choices[0].message.content);
+      userInterface.prompt();
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+});
+app.listen(3000, () => {
+  console.log("start a conversation");
+});
